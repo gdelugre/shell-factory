012a23c96265b886ac8b5005c0c078c11a2799f2
Guillaume Delugré <gdelugre@security-labs.org>
2014-11-28 22:49:33 +0100

    Rakefile: new switch OUTPUT_DEBUG

--- Rakefile.rb
+++ Rakefile.rb
@@ -4,18 +4,23 @@ require 'rake/clean'
 require 'ipaddr'
 
 INCLUDE_DIRS = %w{include include/sysdeps/generic include/ports}
-CFLAGS = "-std=gnu11 -Os -fPIC -fno-common -fno-toplevel-reorder -fomit-frame-pointer -finline-functions -flto -nodefaultlibs -nostdlib #{INCLUDE_DIRS.map{|d| "-I#{d}"}.join(" ")}"
+CFLAGS = "-std=gnu11 -Os -fPIC -fno-common -fno-toplevel-reorder -fomit-frame-pointer -finline-functions -nodefaultlibs -nostdlib #{INCLUDE_DIRS.map{|d| "-I#{d}"}.join(" ")}"
 
 def build(target, *opts)
-    default_opts = [ "CHANNEL", "HOST", "PORT" ]
-    options = default_opts + opts
+    common_opts = [ "CHANNEL", "HOST", "PORT" ]
+    options = common_opts + opts
     defines = ENV.select{|e| options.include?(e)}.map{|k,v| 
         v = [ v.to_i ].pack('v').unpack('n')[0] if k == "PORT" 
         v = [ IPAddr.new(v).to_i ].pack('V').unpack('N')[0] if k == 'HOST'
         "-D#{k}=#{v}"
     }.join(' ')
+    cflags = CFLAGS.dup
 
-    sh "gcc #{CFLAGS} shellcodes/#{target}.c -o bins/#{target}.elf #{defines}"
+    if ENV['OUTPUT_DEBUG'] and ENV['OUTPUT_DEBUG'].to_i == 1
+        sh "gcc -S #{cflags} shellcodes/#{target}.c -o bins/#{target}.S #{defines}"
+    end
+
+    sh "gcc #{cflags} shellcodes/#{target}.c -o bins/#{target}.elf #{defines}"
     sh "objcopy -O binary -j .text -j .rodata bins/#{target}.elf bins/#{target}.bin" 
 end
 
