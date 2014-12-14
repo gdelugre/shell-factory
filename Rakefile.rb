017d99a15bd5c97c226fd420477a31f1c05932ba
Guillaume Delugré <gdelugre@security-labs.org>
2014-12-15 00:58:08 +0100

    Rakefile: CROSS_COMPILE option.

--- Rakefile.rb
+++ Rakefile.rb
@@ -32,7 +32,7 @@ CFLAGS = %w{-std=gnu11
             -nodefaultlibs -nostdlib
          }
 
-def compile(target, output_dir, *opts)
+def compile(target, toolchain, output_dir, *opts)
     common_opts = %w{CHANNEL HOST PORT NO_BUILTIN FORK_ON_ACCEPT REUSE_ADDR}
     options = common_opts + opts
     defines = ENV.select{|e| options.include?(e)}
@@ -44,7 +44,7 @@ def compile(target, output_dir, *opts)
     end
 
     if ENV['OUTPUT_DEBUG'] and ENV['OUTPUT_DEBUG'].to_i == 1
-        sh "gcc -S #{cflags.join(" ")} shellcodes/#{target}.c -o #{output_dir}/#{target}.S #{defines}"
+        sh "#{toolchain}gcc -S #{cflags.join(" ")} shellcodes/#{target}.c -o #{output_dir}/#{target}.S #{defines}"
     end
 
     if ENV['OUTPUT_LIB'] and ENV['OUTPUT_LIB'].to_i == 1
@@ -57,18 +57,23 @@ def compile(target, output_dir, *opts)
         "-D#{k}=#{v}"
     }
 
-    sh "gcc #{cflags.join(' ')} shellcodes/#{target}.c -o #{output_dir}/#{target}.elf #{defines.join(' ')}"
+    sh "#{toolchain}gcc #{cflags.join(' ')} shellcodes/#{target}.c -o #{output_dir}/#{target}.elf #{defines.join(' ')}"
 end
 
-def generate_shellcode(target, output_dir)
-    sh "objcopy -O binary -j .text -j .funcs -j .rodata bins/#{target}.elf #{output_dir}/#{target}.bin" 
+def generate_shellcode(target, toolchain, output_dir)
+    sh "#{toolchain}objcopy -O binary -j .text -j .funcs -j .rodata bins/#{target}.elf #{output_dir}/#{target}.bin" 
 end
 
 def build(target, *opts)
     output_dir = OUTPUT_DIR
+    toolchain = ''
 
-    compile(target, output_dir, *opts)
-    generate_shellcode(target, output_dir)
+    if ENV['CROSS_COMPILE']
+        toolchain = ENV['CROSS_COMPILE'] + '-'
+    end
+
+    compile(target, toolchain, output_dir, *opts)
+    generate_shellcode(target, toolchain, output_dir)
 end
 
 task :readflag do
