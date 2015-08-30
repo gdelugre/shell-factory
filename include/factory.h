49a3ba4bcd7f03c373c275bf9f0b3154719b1350
Guillaume Delugré <guillaume@security-labs.org>
2015-08-30 02:22:10 +0200

    factory.h: remove unused definition

--- include/factory.h
+++ include/factory.h
@@ -22,17 +22,13 @@
 #define SYSTEM_CALL static INLINE
 #define BUILTIN(func) __builtin_ ## func
 
-#ifndef NO_BUILTIN
-#define NO_BUILTIN 0
-#endif
-
 #define STRINGIZE(x) #x
 #define TO_STRING(x) STRINGIZE(x)
 
 #define SHELLCODE_ENTRY NO_RETURN static void Shellcode::entry()
 
 namespace Shellcode {
-    NO_RETURN INLINE static void entry();
+    NO_RETURN FUNCTION void entry();
 }
 
 extern "C" {
