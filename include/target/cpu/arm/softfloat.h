027ca09f5f06f285cfe9a4be26ba459f4a6e079c
Guillaume Delugré <guillaume@security-labs.org>
2016-01-11 03:06:58 +0100

    target/arm: mark softfloat functions helpers with EXPORT_ABI_FUNCTION

--- include/target/cpu/arm/softfloat.h
+++ include/target/cpu/arm/softfloat.h
@@ -3,22 +3,26 @@
 
 extern "C" {
 
+    EXPORT_ABI_FUNCTION
     uint32_t __udivsi3(uint32_t dividend, uint32_t divisor)
     {
         return Math::soft_div(dividend, divisor);
     }
 
+    EXPORT_ABI_FUNCTION
     uint32_t __umodsi3(uint32_t dividend, uint32_t divisor)
     {
         return Math::soft_mod(dividend, divisor);
     }
 
 #if defined(__ARM_EABI__)
+    EXPORT_ABI_FUNCTION
     uint32_t __aeabi_uidiv(uint32_t dividend, uint32_t divisor)
     {
         return __udivsi3(dividend, divisor);
     }
 
+    EXPORT_ABI_FUNCTION
     uint64_t __aeabi_uidivmod(uint32_t dividend, uint32_t divisor)
     {
         uint64_t q = __aeabi_uidiv(dividend, divisor);
