85ed95db9bd75837c4d87f431b9e5018e4614b75
Guillaume Delugré <guillaume@security-labs.org>
2016-01-30 04:36:05 +0100

    target/freebsd: shared mem support

--- include/target/freebsd/pico/ipc.cc
+++ include/target/freebsd/pico/ipc.cc
@@ -10,6 +10,51 @@ namespace Pico {
         Syscall::pipe(fds);
         return BiStream<Pipe>(Pipe(fds[0]), Pipe(fds[1]));
     } 
+
+    namespace Memory {
+
+        template <size_t N>
+        METHOD
+        constexpr key_t ftok(const char *name)
+        {
+            return name[N] ^ (1000003 * ftok<N-1>(name));
+        }
+
+        template <>
+        METHOD
+        constexpr key_t ftok<size_t(-1)>(const char *name)
+        {
+            return 0;
+        }
+
+        template <int N>
+        CONSTRUCTOR SharedRegion::SharedRegion(const char (&name)[N],
+                                               void *base, size_t size,
+                                               int prot, Rights rights)
+        {
+            key_t key = ftok<N>(name); // Compile-time hash of input name.
+
+            shm_obj = Syscall::shmget(key, size, IPC_CREAT | rights.value);
+            void *result = Syscall::shmat(shm_obj, base, SHM_RND);
+            if ( Target::is_error(result) ) {
+                ptr = nullptr;
+                return;
+            }
+
+            ptr = result;
+            region_size = Memory::round_up_page_size(size);
+
+            Syscall::mprotect(ptr, region_size, mmap_prot(prot));
+        }
+
+        DESTRUCTOR SharedRegion::~SharedRegion()
+        {
+            if ( ptr != nullptr ) {
+                Syscall::shmdt(ptr);
+                Syscall::shmctl(shm_obj, IPC_RMID, nullptr);
+            }
+        }
+    }
 }
 
 #endif
--- include/target/freebsd/syscalls/ipc.cc
+++ include/target/freebsd/syscalls/ipc.cc
@@ -1,16 +1,66 @@
 #ifndef SYSCALL_IPC_H_
 #define SYSCALL_IPC_H_
 
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
+
+/*
+ * System calls defined in this file.
+ */
 namespace Syscall {
 
-    SYSTEM_CALL int pipe(int pipefd[2]);
+    SYSTEM_CALL int pipe(int[2]);
+    SYSTEM_CALL int shmget(key_t, size_t, int);
+    SYSTEM_CALL void *shmat(int, const void *, int);
+    SYSTEM_CALL int shmdt(const void *);
+    SYSTEM_CALL int shmctl(int, int, struct shmid_ds *);
 
     SYSTEM_CALL
     int pipe(int pipefd[2])
     {
         return DO_SYSCALL(pipe, pipefd);
+    }
+
+    SYSTEM_CALL
+    int shmget(key_t key, size_t size, int shmflg)
+    {
+        #if SYSCALL_EXISTS(shmget)
+        return DO_SYSCALL(shmget, key, size, shmflg);
+        #else
+        return DO_SYSCALL(shmsys, 3, key, size, shmflg);
+        #endif
+    }
+
+    SYSTEM_CALL
+    void *shmat(int shmid, const void *shmaddr, int shmflg)
+    {
+        #if SYSCALL_EXISTS(shmat)
+        return (void *) DO_SYSCALL(shmat, shmid, shmaddr, shmflg);
+        #else
+        return (void *) DO_SYSCALL(shmsys, 0, shmid, shmaddr, shmflg);
+        #endif
+    }
+
+    SYSTEM_CALL
+    int shmdt(const void *shmaddr)
+    {
+        #if SYSCALL_EXISTS(shmdt)
+        return DO_SYSCALL(shmdt, shmaddr);
+        #else
+        return DO_SYSCALL(shmsys, 2, shmaddr);
+        #endif
+    }
+
+    SYSTEM_CALL
+    int shmctl(int shmid, int cmd, struct shmid_ds *buf)
+    {
+        #if SYSCALL_EXISTS(shmctl)
+        return DO_SYSCALL(shmctl, shmid, cmd, buf);
+        #else
+        return DO_SYSCALL(shmsys, 4, shmid, cmd, buf);
+        #endif
     }
 }
 
