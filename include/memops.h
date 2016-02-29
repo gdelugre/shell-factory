927f57e979e8effe9207bbfda9600c5fca5d1666
Guillaume Delugré <guillaume@security-labs.org>
2016-03-01 00:31:31 +0100

    include/memops: added strlen

--- include/memops.h
+++ include/memops.h
@@ -68,6 +68,17 @@ extern "C" {
 
         return s;
     }
+
+    EXPORT_ABI_FUNCTION
+    size_t strlen(const char *s)
+    {
+        size_t len = 0;
+
+        while ( s[len] )
+            len++;
+
+        return len;
+    }
 }
 
 #endif
