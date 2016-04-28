b20c3972b4197faf3b0e6fa245c6fb8fb18b9562
Guillaume Delugré <guillaume@security-labs.org>
2016-04-29 00:34:45 +0200

    pico/format: implementation of snprintf

--- include/pico/format.h
+++ include/pico/format.h
@@ -6,10 +6,12 @@ namespace Pico {
     template <typename T>
     FUNCTION_NOINLINE
     size_t format_ltoa(T& dest,
+                       size_t current_sz,
+                       size_t max_sz,
                        unsigned long value, int radix,
                        bool upcase,
                        char pad, int pad_sz,
-                       size_t (*output)(T&, const void *, size_t))
+                       size_t (*output)(T&, const void *, size_t, size_t, size_t))
     {
         size_t count = 0;
         int nr_digits = 0;
@@ -28,17 +30,17 @@ namespace Pico {
         } while ( value != 0 );
 
         for ( int i = 0; i < (pad_sz - nr_digits); i++ )
-            count += output(dest, &pad, sizeof(pad));
+            count += output(dest, &pad, sizeof(pad), current_sz, max_sz);
 
         for ( int i = 0; i < nr_digits; i++ )
-            count += output(dest, &str[nr_digits - i - 1], 1);
+            count += output(dest, &str[nr_digits - i - 1], 1, current_sz, max_sz);
 
         return count;
     }
 
     template <typename T>
     FUNCTION
-    int vformat(T& dest, const char *format, va_list ap, size_t (*output)(T&, const void *, size_t))
+    int vformat(T& dest, size_t max_size, const char *format, va_list ap, size_t (*output)(T&, const void *, size_t, size_t, size_t))
     {
         char *fmt = const_cast<char *>(format);
         int escape = 0;
@@ -52,7 +54,7 @@ namespace Pico {
             c = *fmt++;
             if ( !escape && c != '%' )
             {
-                result += output(dest, &c, 1);
+                result += output(dest, &c, 1, result, max_size);
             }
             else if ( !escape && c == '%' )
             {
@@ -72,12 +74,12 @@ namespace Pico {
                 switch ( c )
                 {
                     case '%':
-                        result += output(dest, &c, 1); break;
+                        result += output(dest, &c, 1, result, max_size); break;
 
                     case 'c':
                     {
                         char param_chr = va_arg(ap, int);
-                        result += output(dest, &param_chr, sizeof(param_chr));
+                        result += output(dest, &param_chr, sizeof(param_chr), result, max_size);
                         break;
                     }
 
@@ -87,11 +89,11 @@ namespace Pico {
                     {
                         int param_word = va_arg(ap, int);
                         if ( c != 'u' && param_word < 0 ) {
-                            result += output(dest, "-", 1);
+                            result += output(dest, "-", 1, result, max_size);
                             param_word = -param_word;
                         }
 
-                        result += format_ltoa(dest, param_word, 10, false, pad, pad_sz, output);
+                        result += format_ltoa(dest, result, max_size, param_word, 10, false, pad, pad_sz, output);
                         break;
                     }
 
@@ -101,21 +103,21 @@ namespace Pico {
                         size_t param_str_sz = param_str.length();
 
                         for ( int i = 0; i < pad_sz - static_cast<int>(param_str_sz); i++ )
-                            result += output(dest, &pad, sizeof(pad));
-                        result += output(dest, param_str.pointer(), param_str_sz);
+                            result += output(dest, &pad, sizeof(pad), result, max_size);
+                        result += output(dest, param_str.pointer(), param_str_sz, result, max_size);
                         escape = 0;
                         break;
                     }
 
                     case 'p':
                         static char hex_prefix[] = { '0', 'x' };
-                        result += output(dest, hex_prefix, sizeof(hex_prefix));
+                        result += output(dest, hex_prefix, sizeof(hex_prefix), result, max_size);
 
                     case 'x':
                     case 'X':
                     {
                         unsigned long param_word = va_arg(ap, unsigned long);
-                        result += format_ltoa(dest, param_word, 16, (c == 'X'), pad, pad_sz, output);
+                        result += format_ltoa(dest, result, max_size, param_word, 16, (c == 'X'), pad, pad_sz, output);
                         break;
                     }
 
@@ -136,17 +138,50 @@ namespace Pico {
     int vsprintf(char *str, const char *format, va_list ap)
     {
 
-        size_t (*output_func)(char *&, const void *, size_t) =
-            [](char *& ptr, const void *buffer, size_t n) -> size_t {
+        size_t (*output_func)(char *&, const void *, size_t, size_t, size_t) =
+            [](char *& ptr, const void *buffer, size_t n, size_t, size_t) -> size_t {
                 Memory::copy(ptr, buffer, n);
                 ptr += n;
 
                 return n;
             };
         char *output = str;
-        int count = vformat(output, format, ap, output_func);
+        int count = vformat(output, SIZE_MAX, format, ap, output_func);
 
         str[count] = '\0';
+        return count;
+    }
+
+    FUNCTION
+    int vsnprintf(char *str, size_t max_size, const char *format, va_list ap)
+    {
+        size_t (*output_func)(char *&, const void *, size_t, size_t, size_t) =
+            [](char *& ptr, const void *buffer, size_t n, size_t current_sz, size_t max_sz) -> size_t {
+                size_t nb;
+
+                if ( current_sz >= max_sz )
+                    return n;
+
+                if ( current_sz + n >= max_sz )
+                    nb = max_sz - current_sz - 1;
+                else
+                    nb = n;
+
+                if ( nb > 0 ) {
+                    Memory::copy(ptr, buffer, nb);
+                    ptr += nb;
+                }
+
+                return n;
+            };
+        char *output = str;
+        int count = vformat(output, max_size, format, ap, output_func);
+
+        if ( count >= max_size )
+            str[max_size] = '\0';
+        else
+            str[count] = '\0';
+
         return count;
     }
 
@@ -158,6 +193,19 @@ namespace Pico {
 
         va_start(ap, format);
         cnt = vsprintf(str, format, ap);
+        va_end(ap);
+
+        return cnt;
+    }
+
+    FUNCTION_NOINLINE __attribute__((format (printf, 3, 4)))
+    int snprintf(char *str, size_t max_size, const char *format, ...)
+    {
+        int cnt;
+        va_list ap;
+
+        va_start(ap, format);
+        cnt = vsnprintf(str, max_size, format, ap);
         va_end(ap);
 
         return cnt;
@@ -195,12 +243,12 @@ namespace Pico {
     METHOD
     int Stream<Io>::vprintf(const char *format, va_list ap)
     {
-        size_t (*output_func)(Stream<Io>&, const void *, size_t) =
-            [](Stream<Io>& s, const void *buffer, size_t n) -> size_t {
+        size_t (*output_func)(Stream<Io>&, const void *, size_t, size_t, size_t) =
+            [](Stream<Io>& s, const void *buffer, size_t n, size_t, size_t) -> size_t {
                 return s.write(buffer, n);
             };
 
-        int count = vformat(*this, format, ap, output_func);
+        int count = vformat(*this, SIZE_MAX, format, ap, output_func);
         return count;
     }
 
