b29dccc0129ae6ccf7b57566e00bd0e97b0d4eaa
Guillaume Delugré <guillaume@security-labs.org>
2016-05-11 23:58:36 +0200

    factory: attribute ALWAYS_INLINE

--- include/factory.h
+++ include/factory.h
@@ -4,27 +4,31 @@
 // Unneeded and may conflict with our function definitions.
 #undef _FORTIFY_SOURCE
 
+#define ALWAYS_INLINE inline __attribute__((always_inline))
+#define NOINLINE __attribute__((noinline))
+
 #if defined(RELAX_INLINE) && (RELAX_INLINE == 1)
 #define INLINE inline
 #elif defined(RELAX_INLINE) && (RELAX_INLINE == 2)
 #define INLINE
 #elif defined(RELAX_INLINE) && (RELAX_INLINE == 3)
-#define INLINE __attribute__((noinline))
+#define INLINE NOINLINE
 #else
-#define INLINE inline __attribute__((always_inline))
+#define INLINE ALWAYS_INLINE
 #endif
 
 #define NO_RETURN [[noreturn]]
 #define CONSTRUCTOR INLINE
 #define DESTRUCTOR INLINE
-#define METHOD_NOINLINE __attribute__((noinline))
+#define METHOD_NOINLINE NOINLINE
 #define METHOD INLINE
 #define VIRTUAL_METHOD virtual METHOD
 #define FUNCTION static METHOD
 #define FUNCTION_NOINLINE static METHOD_NOINLINE
+#define FUNCTION_INLINE static ALWAYS_INLINE
 #define EXPORT_ABI_FUNCTION __attribute__((visibility("internal")))
 #define SYSTEM_CALL static INLINE
-#define SYSTEM_CALL_INLINE static inline __attribute__((always_inline))
+#define SYSTEM_CALL_INLINE static ALWAYS_INLINE
 #define UNUSED __attribute__((unused))
 #define USED __attribute__((used))
 #define PURE __attribute__((pure))
