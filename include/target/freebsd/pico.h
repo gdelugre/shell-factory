de7f355af0425d1eac02978de3fe5140adf21205
Guillaume Delugré <guillaume@security-labs.org>
2017-02-15 22:50:22 +0100

    target/freebsd: missing posix include

--- include/target/freebsd/pico.h
+++ include/target/freebsd/pico.h
@@ -1,10 +1,13 @@
 #ifndef FREEBSD_PICOLIB_H_
 #define FREEBSD_PICOLIB_H_
 
-/* Import FreeBSD syscalls. */
+// Import FreeBSD syscalls.
 #include <target/freebsd/syscalls.h>
 
-/* Target implementation. */
+// Import POSIX implementation.
+#include <target/posix/pico.h>
+
+// Target implementation.
 #include "pico/concurrency.cc"
 #include "pico/memory.cc"
 #include "pico/stream.cc"
